e78db0f09f2876060c7ed01a5c5a9b33c2c09e79
Matthew Larkin <matthewlarkin8@gmail.com>
2022-07-25 16:41:37 -0400

    Created static method roll_dice for the GameLogic class. It takes in an int and returns a tuple containing int number of dice rolls. Tested function with int being all values 1-6. Confirmed all tests are passing.

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,2 +1,12 @@
+import random
+
 class GameLogic:
-    pass
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def roll_dice(num):
+        rolls = []
+        while len(rolls) < num:
+            rolls.append(random.randint(1, 6))
+        return tuple(rolls)
