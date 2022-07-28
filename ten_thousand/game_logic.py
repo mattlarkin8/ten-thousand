ea8a9b6eb95a2d8ed6c8803673defe3b8e027216
Matthew Larkin <matthewlarkin8@gmail.com>
2022-07-27 20:49:31 -0400

    Checked each different scoring category individually to confirm it functioned properly. Found and corrected many bugs within my calculate score function. I also converted the user input to int from a str. This resolved at least one problem I had found.

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -3,12 +3,13 @@ from collections import Counter
 
 class Banker:
     score = 0
+
     def __init__(self):
         pass
 
     @staticmethod
     def add_to_score(points):
-        Banker.score += points
+        Banker.score = Banker.score + points
 
     @staticmethod
     def get_score():
@@ -34,7 +35,9 @@ class GameLogic:
     @staticmethod
     def calculate_score(tup):
         score = 0
-        roll = Counter(tup).most_common()
+        str_list = list(tup)
+        nums = [int(num) for num in str_list]
+        roll = Counter(nums).most_common()
 
         if len(tup) == 0:
             return score
@@ -46,92 +49,90 @@ class GameLogic:
         #6 of a kind
         if roll[0][1] == 6:
             if roll[0][0] == 1:
-                score += 4000
-                return score
-            score += int(roll[0][0]) * 400
-            return score
+                return 4000
+            return int(roll[0][0]) * 400
 
         #5 of a kind
-        elif roll[0][1] == 5:
-            if roll[0][0] == 1:
-                score += 3000
-
-            elif len(roll) > 1:
+        if roll[0][1] == 5:
+            if len(roll) == 2:
                 if roll[1][0] == 5:
                     score += 50
 
-                elif roll[1][0] == 1:
+                if roll[1][0] == 1:
                     score += 100
 
-            else:
-                score += int(roll[0][0]) * 300
-                return score
+            if roll[0][0] == 1:
+                return score + 3000
+
+            score += int(roll[0][0]) * 300
+            return score
 
         #4 of a kind
-        elif roll[0][1] == 4:
-            if roll[0][0] == 1:
-                score += 2000
-
-            elif len(roll) > 1:
+        if roll[0][1] == 4:
+            if len(roll) >= 2:
                 if roll[1][0] == 5:
                     score += 50 * roll[1][1]
 
-                elif roll[1][0] == 1:
+                if roll[1][0] == 1:
                     score += 100 * roll[1][1]
 
-                elif roll[2][0] == 5:
+            if len(roll) == 3:
+                if roll[2][0] == 5:
                     score += 50 * roll[2][1]
 
-                elif roll[2][0] == 1:
+                if roll[2][0] == 1:
                     score += 100 * roll[2][1]
 
-            else:
-                score += int(roll[0][0]) * 200
-                return score
+            if roll[0][0] == 1:
+                return score + 2000
+
+            score += int(roll[0][0]) * 200
+            return score
 
         # double 3 of a kind
-        elif len(roll) == 2:
+        if len(roll) == 2:
             if roll[0][1] == 3 and roll[1][1] == 3:
-                if roll[0][0] == 1:
-                    score += 1000
-                    return score + roll[1][0] * 100
-
-                if roll[1][0] == 1:
-                    score += 1000
-                    return score + roll[0][0] * 100
+                # if roll[0][0] == 1:
+                #     score += 1000
+                #     return score + roll[1][0] * 100
+                #
+                # if roll[1][0] == 1:
+                #     score += 1000
+                #     return score + roll[0][0] * 100
 
                 return roll[0][0] * 100 + roll[1][0] * 100
 
         # 3 of a kind
-        elif roll[0][1] == 3:
-            if roll[0][0] == 1:
-                score += 1000
-
-            elif len(roll) > 1:
+        if roll[0][1] == 3:
+            if len(roll) >= 2:
                 if roll[1][0] == 5:
                     score += 50 * roll[1][1]
 
-                elif roll[1][0] == 1:
+                if roll[1][0] == 1:
                     score += 100 * roll[1][1]
 
-                elif roll[2][0] == 5:
+            if len(roll) >= 3:
+                if roll[2][0] == 5:
                     score += 50 * roll[2][1]
 
-                elif roll[2][0] == 1:
+                if roll[2][0] == 1:
                     score += 100 * roll[2][1]
 
-                elif roll[3][0] == 5:
+            if len(roll) == 4:
+                if roll[3][0] == 5:
                     score += 50 * roll[3][1]
 
-                elif roll[3][0] == 1:
+                if roll[3][0] == 1:
                     score += 100 * roll[3][1]
 
-            else:
-                score += int(roll[0][0]) * 100
-                return score
+            if roll[0][0] == 1:
+                return score + 1000
+
+            score += int(roll[0][0]) * 100
+            return score
 
         # 3 pairs
-        elif len(roll) > 2:
+        if len(roll) > 2:
             if roll[0][1] and roll[1][1] and roll[2][1] == 2:
                 score += 1500
                 return score
@@ -152,29 +153,16 @@ class GameLogic:
         #
         #     return score
 
-        # pair 1's
-        elif roll[0][1] == 2 and roll[0][0] == 1:
-            score += 200
-
-        # pair 5's
-        elif roll[0][1] == 2 and roll[0][0] == 5:
-            score += 100
-
-        elif len(roll) > 1:
-            if roll[1][1] == 2 and roll[1][0] == 5:
-                score += 100
-
-        # single 1's
-        elif roll[0][0] == 1:
-            score += 100
-
-        # single 5's
-        elif roll[0][0] == 5:
-            score += 50
+        # handle single 1's and 5's
+        one_counter = 0
+        five_counter = 0
+        for num in nums:
+            if num == 1:
+                one_counter += 1
+            if num == 5:
+                five_counter += 1
+        score += one_counter * 100
+        score += five_counter * 50
 
         # default/catch all return
         return score
-
-    @staticmethod
-    def test_roller():
-        return 4, 2, 6, 4, 6, 5
